9ca63ce303926e20aac65606010b12dfe4c56f7b
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-03-17 14:06:03 -0500

    finished second part of select_exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,1 +1,25 @@
 USE codeup_test_db;
+
+SELECT name
+FROM albums
+WHERE artist = 'Pink Floyd';
+
+SELECT release_date
+FROM albums
+WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT genre
+FROM albums
+WHERE name  = 'Nevermind';
+
+SELECT name
+FROM albums
+WHERE release_date BETWEEN '1990' and '2000';
+
+SELECT name
+FROM albums
+WHERE sales < 20.0;
+
+SELECT name
+FROM albums
+WHERE genre = 'Rock';
