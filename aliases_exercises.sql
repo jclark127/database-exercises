4ae8cb0495cb316755bfef231e6833aaa8f6af64
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-03-21 11:58:06 -0500

    fixed typo in alaises exercise

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,5 +1,5 @@
 USE employees;
 
-SELECT CONCAT(emp_no,' - ',first_name, ',', last_name) AS full_name, birth_date
+SELECT CONCAT(emp_no,' - ',last_name, ',', first_name) AS full_name, birth_date
 FROM employees
 LIMIT 10;
