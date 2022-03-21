fad495f52d11e8739ff36802a63cd1b28bfe72e0
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-03-21 11:46:10 -0500

    finished first part of group by exercise

---
+++ group_by_exercises.sql
@@ -0,0 +1,12 @@
+USE employees;
+
+SELECT DISTINCT title
+FROM titles;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND (last_name LIKE '%E')
+GROUP BY first_name,last_name
+ORDER BY last_name;
+
