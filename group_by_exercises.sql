bfc248f28d2ccffebb3688055f02ec61a63bc48d
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-03-21 11:51:02 -0500

    finished 3rd part of group by exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -10,3 +10,9 @@ WHERE last_name LIKE 'E%'
 GROUP BY first_name,last_name
 ORDER BY last_name;
 
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%Q%'
+AND not (last_name LIKE '%QU%')
+GROUP BY last_name;
+
